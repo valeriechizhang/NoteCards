4ecfb9c81b58b49823e8c0ab9f79df37b2434d48
valeriechizhang <chizhang@bu.edu>
2017-02-22 22:01:12 -0800

    fourth feb22 commit

--- main.py
+++ main.py
@@ -199,23 +199,17 @@ def gconnect():
         return response
     # Obtain authorization code
     code = request.data
-    print '1'
     try:
         # Upgrade the authorization code into a credentials object
-        print '@@@@@'
         oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
-        print '****'
         oauth_flow.redirect_uri = 'postmessage'
-        print '****'
         credentials = oauth_flow.step2_exchange(code)
-        print '****'
     except FlowExchangeError:
-        print '#####'
         response = make_response(
             json.dumps('Failed to upgrade the authorization code.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-    print '2'
+
     # Check that the access token is valid.
     access_token = credentials.access_token
     url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
@@ -226,7 +220,7 @@ def gconnect():
         response = make_response(json.dumps(result.get('error')), 500)
         response.headers['Content-Type'] = 'application/json'
         return response
-    print '3'
+
     # Verify that the access token is used for the intended user.
     gplus_id = credentials.id_token['sub']
     if result['user_id'] != gplus_id:
@@ -234,7 +228,7 @@ def gconnect():
             json.dumps("Token's user ID doesn't match given user ID."), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-    print '4'
+
     # Verify that the access token is valid for this app.
     if result['issued_to'] != CLIENT_ID:
         response = make_response(
@@ -242,14 +236,14 @@ def gconnect():
         print "Token's client ID does not match app's."
         response.headers['Content-Type'] = 'application/json'
         return response
-    print '5'
+
     stored_credentials = login_session.get('credentials')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_credentials is not None and gplus_id == stored_gplus_id:
         response = make_response(json.dumps('Current user is already connected.'), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
-    print '6'
+
     # Store the access token in the session for later use.
     login_session['access_token'] = credentials.access_token
     login_session['gplus_id'] = gplus_id
